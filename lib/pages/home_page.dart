54f8200a6bfb7619299e324bf338a5fe85972268
Parmesh Mahore <parmeshtoyou@gmail.com>
2022-07-18 23:23:14 +0530

    added list and dummy items

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widget/drawer.dart';
 
-class HomePage extends StatelessWidget {
+import '../widget/item_widget.dart';
 
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => products[0]);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog"),
       ),
       drawer: AppDrawer(),
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context, index) {
+          return ItemWidget(
+            key: null,
+            item: dummyList[index],
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/widget/item_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Card(
+        child: ListTile(
+          leading: Image.network(item.image),
+          title: Text(item.name),
+          subtitle: Text(item.desc),
+          trailing: Text("\$${item.price.toString()}"),
+        ),
+      ),
+    );
+  }
+}
